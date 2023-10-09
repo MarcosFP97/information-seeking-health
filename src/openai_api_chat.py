08c6c64e1cf43573bb01d58b2cd78fd2ae0d3f9f
marcos.fernandez.pichel <marcosfernandez.pichel@usc.es>
2023-10-09 17:38:50 +0200

    First sytem role runs

--- src/openai_api_chat.py
+++ src/openai_api_chat.py
@@ -1,72 +1,172 @@
-import os
 import re
 import openai
-import typer
-import time
+import pandas as pd
 import argparse
 import xml.etree.ElementTree as ET
-API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+import tiktoken
 
-def load_prompt(path:str):
-  with open(path) as f:
-    content = f.read()
-  return content
+API_KEY="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"
 
-def load_answers(path:str):
-  answers = dict()
-  root = ET.parse(path).getroot()
+'''
+This method loads a context from file.
+Args:
+    - path: path to the file containing the context
+Return:
+    - returns the context
+'''
+def load_context(
+  context:str
+)-> str:
+  if context:
+    with open('../contexts/'+context+'.txt') as f:
+      context = f.read()
+  return context
+
+
+'''
+This method generates a formatted prompt to input the model
+Args:
+    - message: main message to be included in the prompt
+Return:
+    - returns the formatted prompt
+'''
+def get_prompt(
+    context: str,
+    system:bool,
+    question:str
+) -> str:
+  if system:
+    prompt = (
+        f'{context}\n'
+        f'You are a helpful medical assistant.\n'
+        f'{question}\n'
+    )
+  else:
+    prompt = (
+        f'{context}\n'
+        f'You are a helpful medical assistant.\n'
+        f'{question}\n'
+    )
+
+  return prompt
+
+
+'''
+This method loads the ground truth values for health questions from file.
+Args:
+    - path: path to the file containing the ground truth questions
+Return:
+    - returns a dictionary with the questions as keys and the answers as values
+'''
+def load_answers(
+  year:int
+)-> dict:
+  answers = {}
+  root = ET.parse('../evaluation/misinfo-resources-'+str(year)+'/topics/misinfo-'+str(year)+'-topics.xml').getroot()
   for topic in root.findall('topic'):
-    query = topic.find("question").text 
-    answer = topic.find("answer").text 
+    if str(year)=="2022":
+      query = topic.find("question").text 
+      answer = topic.find("answer").text 
+    elif str(year)=="2021":
+      query = topic.find("description").text 
+      answer = topic.find("stance").text
+      if answer=="helpful":
+        answer = 'yes'
+      else:
+        answer = 'no'
+    elif str(year)=="2020":
+      query = topic.find("description").text 
+      answer = topic.find("answer").text
     answers[query.rstrip()] = answer
   return answers
 
-def ask_gpt(model:str,
-            prompt: str,
-            answers: dict):
-  n = len(answers)
-  hits = 0
-  for k, v in answers.items():
-    gpt_prompt =  '\nQ:Will wearing an ankle brace help heal achilles tendonitis?\nA:No\nQ:Does yoga improve the management of asthma?\nA:Yes\nQ:Is starving a fever effective?\nA:No\n' + prompt +'\n'+ k + ' The answer must be Yes or No' 
-    print(gpt_prompt)
 
-    response = openai.ChatCompletion.create(
-      model=model,
-      messages=[
-          {"role": "system", "content": "You are a chatbot"},
-          {"role": "user", "content": gpt_prompt},
-      ]
-    )
+'''
+This method evaluates model's answer against ground truth values
+Args:
+    - response: model's answer to the question
+    - answer: ground truth value
+Return:
+    - returns 1 if there is a math and 0 otherwise
+'''
+def evaluate(
+    response:str,
+    answer:str
+)-> int:
+    hit=0
+    response = response.lower()
 
-    result = ''
-    for choice in response.choices:
-        result += choice.message.content
+    m = re.search(r'\byes\b|\bno\b', response)
+    if m and m.group().strip()==answer:
+        hit=1
+    return hit
 
-    print(result)
-    
-    if model == "text-davinci-002": #### if the model is less obedient
-      m = re.search(r'\bno definitive\b|\bno clear\b|\bno certain\b|\bno consensus\b', result.lower())
-      if m:
-        continue
+'''
+This method asks a LLM for the answer to a suite of health-related questions
+Args:
+    - model: model_name
+    - prompt: context
+    - answers: dict containing the questions plus their answers
+Return:
+    - returns the accuracy value for all the answers
+'''
+def ask(
+  model:str,
+  eval:dict,
+  context:str,
+  expert: str, 
+  year: int,
+  syst:bool=True,
+)-> float:
+  number_questions = len(eval)
+  
+  if syst:
+    outputfile = expert + str(year) + '_s.txt'
+  else:
+    outputfile = expert + str(year) + '.txt'
 
-    m = re.search(r'\byes\b|\bno\b', result.lower())
-    if m and m.group().strip()==v:
-      hits+=1
-      print(hits)
-    
-    print()
-    print()
+  with open('../outputs/zero-shot/'+model+'/'+outputfile, 'w+') as f:
+    hits = 0
+    for k, v in eval.items():
+        prompt = get_prompt(context, syst, k)
+        print(prompt)
+        f.write(prompt+'\n')
 
-  print("Accuracy:", hits/n)    
+        enc = tiktoken.encoding_for_model(model)
+        logit_bias = {enc.encode("yes")[0]:50, enc.encode("no")[0]:50}
+        response = openai.ChatCompletion.create(
+            model=model,
+            messages=[
+                {"role": "system", "content": "You are a chatbot"},
+                {"role": "user", "content": prompt},
+            ],
+            logit_bias=logit_bias,
+            max_tokens=1,
+            temperature=0 # to ensure reproducibility
+        )
+
+        result = ''
+        for choice in response.choices:
+            result += choice.message.content    
+        f.write(result+'\n')
+        print(result)
+        hit = evaluate(result,v)
+        if hit!=0:
+            hits+=hit
+            f.write(str(hits)+'\n')
+            print(hits)
+
+    accuracy =  hits/number_questions
+    f.write("Accuracy:"+ str(accuracy))
+    return accuracy
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("model", nargs='?', default="gpt-4")
-  parser.add_argument("prompt", nargs='?', default="prompts/usc.txt")
-  parser.add_argument("topics", nargs='?', default="evaluation/misinfo-resources-2022/topics/misinfo-2022-topics.xml")
-  args = parser.parse_args()
-  openai.api_key = API_KEY
-  prompt = load_prompt(args.prompt)
-  answers = load_answers(args.topics)
-  ask_gpt(args.model, prompt, answers)
-  
+    openai.api_key = API_KEY
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model", nargs='?', default="gpt-3.5-turbo")
+    parser.add_argument("context", nargs='?', default="") 
+    parser.add_argument("year", nargs='?', default=2022)
+    args = parser.parse_args()
+    context = load_context(args.context)
+    eval = load_answers(args.year)
+    print(ask(args.model, eval, context, args.context, args.year))
